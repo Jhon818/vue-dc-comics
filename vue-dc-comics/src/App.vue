d9e5be2e0e5c8bb3c5dd425813db82230f393c6c
giov <pintogiovanni818@gmail.com>
2021-09-20 15:55:24 +0200

    scaffolding

--- vue-dc-comics/src/App.vue
+++ vue-dc-comics/src/App.vue
@@ -1,24 +1,17 @@
 <template>
   <div id="app">
-   <Header />
-
-   <Main />
-
-  <Footer />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-import Main from "@/components/Main.vue";
-import Footer from "@/components/Footer.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    Header,
-    Main,
-    Footer
+    HelloWorld,
   },
 };
 </script>
@@ -31,5 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-};
+}
 </style>
