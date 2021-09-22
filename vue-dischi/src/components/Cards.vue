9cc46b31d011355089acc627239f68baa9d1cdfd
giov <pintogiovanni818@gmail.com>
2021-09-22 16:39:56 +0200

    predisposizione header

---
+++ vue-dischi/src/components/Cards.vue
@@ -0,0 +1,19 @@
+<template>
+<section class="conatiner">
+ <ul class="row">
+      <li>
+
+      </li>
+  </ul>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'Cards',
+}
+</script>
+
+<style>
+
+</style>
