e67ea4dfbd420d3c0daa829afe76f12285672916
giov <pintogiovanni818@gmail.com>
2021-09-22 15:07:30 +0200

    scaffholding

---
+++ live-coding/vue-cli-rick-and-morty/rick-morty/src/main.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import App from "~@fontsource/montserrat/700.css";
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");
