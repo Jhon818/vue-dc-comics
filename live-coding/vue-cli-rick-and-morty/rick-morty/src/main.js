e99c54fe0eefa123cc510d2bac0b4bda0265f750
giov <pintogiovanni818@gmail.com>
2021-09-23 19:59:10 +0200

    pulizia

--- live-coding/vue-cli-rick-and-morty/rick-morty/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-
-import App from "~@fontsource/montserrat/700.css";
-Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
